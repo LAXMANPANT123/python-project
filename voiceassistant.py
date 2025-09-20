4a9c370028f796d172628262478d79883b906cc6
LAXMANPANT123 <laxmanpant32@gmail.com>
2025-09-20 08:26:14 +0530

    vrek

--- voiceassistant.py
+++ voiceassistant.py
@@ -1,8 +1,20 @@
+import datetime
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
 import os
 import time
 import playsound
 import speech_recognition as sr
 from gtts import gTTS
+
+
+SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 
 def speak(text):
     tts = gTTS(text=text, lang="en")
@@ -27,6 +39,67 @@ text = get_audio()
 
 if "hello" in text:
     speak("hello, how are u i am good and what's new")
+
+    
+
+
+
+
+def main():
+  """Shows basic usage of the Google Calendar API.
+  Prints the start and name of the next 10 events on the user's calendar.
+  """
+  creds = None
+  
+  if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+  try:
+    service = build("calendar", "v3", credentials=creds)
+
+    
+    now = datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
+    print("Getting the upcoming 10 events")
+    events_result = (
+        service.events()
+        .list(
+            calendarId="primary",
+            timeMin=now,
+            maxResults=10,
+            singleEvents=True,
+            orderBy="startTime",
+        )
+        .execute()
+    )
+    events = events_result.get("items", [])
+
+    if not events:
+      print("No upcoming events found.")
+      return
+
+    
+    for event in events:
+      start = event["start"].get("dateTime", event["start"].get("date"))
+      print(start, event["summary"])
+
+  except HttpError as error:
+    print(f"An error occurred: {error}")
+
+
+if __name__ == "__main__":
+  main()
 
 
 
