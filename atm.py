f279829e2b883655823a2223093cd7248d7d8949
LAXMANPANT123 <laxmanpant32@gmail.com>
2025-09-24 00:17:12 +0530

    add

--- atm.py
+++ atm.py
@@ -1,32 +1,60 @@
-def show_balance():
-    pass
+def show_balance(balance):
+    print(f"your balance is ${balance:2f}")
 
 def deposit():
-    pass
+    amount = float(input("enter an amount to be deposited"))
 
-def withdraw():
-    pass
-
-balance = 0
-is_running = True
-
-while is_running:
-    print("banking program")
-    print("1.showbalance")
-    print("2.deposit")
-    print("3.withdraw")
-    print("4.exit")
-
-
-    choice = input("enter your choice (1-4): ")
-
-    if choice == '1':
-        show_balance()
-    elif choice == '2':
-        deposit()
-    elif choice == '3':
-        withdraw()
-    elif choice == '4':
-        is_running = False
+    if amount < 0:
+        print("thats not a valid amount")
+        return 0
     else:
-        print("that is not valid choice")
+        return amount
+
+def withdraw(balance):
+    amount = float(input("enter amount to be withdrawn: "))
+
+    if amount > balance:
+        print("insufficient funds")
+        return 0
+    elif amount < 0:
+        print("amount must be greater than 0")
+        return 0
+    else:
+        return amount
+    
+
+def main():
+    balance = 0
+    is_running = True
+
+    while is_running:
+
+        print("****************")
+        print("banking program")
+        print("****************")
+        print("1.showbalance")
+        print("****************")
+        print("2.deposit")
+        print("****************")
+        print("3.withdraw")
+        print("****************")
+        print("4.exit")
+
+
+        choice = input("enter your choice (1-4): ")
+
+        if choice == '1':
+            show_balance(balance)
+        elif choice == '2':
+            balance +=deposit()
+        elif choice == '3':
+            balance -=withdraw()
+        elif choice == '4':
+            is_running = False
+        else:
+            print("that is not valid choice")
+
+    print("thank you have a nice day")
+
+
+main()
